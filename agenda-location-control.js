de3a02885dc8f146c09d5c2f4b1651e3ebc9da62
brunours <brunours@users.noreply.github.com>
2026-08-29 09:31:35 +0100

    Remove Safari touch workaround from weather picker

--- agenda-location-control.js
+++ agenda-location-control.js
@@ -1,13 +1,7 @@
 (function(){
 'use strict';
 var core=document.createElement('script');
-core.src='agenda-location-control-core.js?v=20260829c';
+core.src='agenda-location-control-core.js?v=20260829d';
 core.async=false;
-core.onload=function(){
-  var fix=document.createElement('script');
-  fix.src='agenda-location-touch-fix.js?v=20260829c';
-  fix.async=false;
-  document.head.appendChild(fix);
-};
 document.head.appendChild(core);
 })();
