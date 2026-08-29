515d6543aa62e460036911339afaeb316a0068b5
brunours <brunours@users.noreply.github.com>
2026-08-29 10:27:18 +0100

    Refresh weather location control for agenda timezone option

--- agenda-location-control.js
+++ agenda-location-control.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
 var core=document.createElement('script');
-core.src='agenda-location-control-core.js?v=20260829d';
+core.src='agenda-location-control-core.js?v=20260829e';
 core.async=false;
 document.head.appendChild(core);
 })();
